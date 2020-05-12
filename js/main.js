53d16fed6abe9e0f086f3c3dcae20d827586f74b
Christian C. Romero <id.32@outlook.com>
2020-05-12 21:59:33 +0200

    Important and completed filters

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+const taskSearch = document.querySelector('#task-search')
+const optionsContainer = document.querySelector('.options')
+const optionsNewList = document.querySelector('.options__newlist')
 const mainContainer = document.querySelector('main')
-
+const tasksHeader = document.querySelector('.tasks__header')
 const tasksContainer = document.querySelector('.tasks__container')
-
 const newTaskDiv = document.querySelector('.new-task')
 const newTaskTitle = document.querySelector('#new-task-title')
 const newTaskTitleWarning = document.querySelector('#new-task-title-warning')
@@ -14,33 +16,42 @@ const newTaskList = document.querySelector('#new-task-list')
 const newTaskAccept = document.querySelector('#new-task-accept')
 const newTaskCancel = document.querySelector('#new-task-cancel')
 
-const tasks = loadTasks()
+const data = loadLocalStorageData()
 createTasksHTML()
 
 // Functions
-function loadTasks() {
-  const data = localStorage.getItem('qNPXpg7Q-tasks')
-  return data ? JSON.parse(data) : []
+function loadLocalStorageData() {
+  const data = localStorage.getItem('todolist-qNPXpg7Q')
+  return data ? JSON.parse(data) : {lists: ['Default'], tasks: []}
 }
 
-function storeTasks(task) {
-  if (task) tasks.push(task)
-  localStorage.setItem('qNPXpg7Q-tasks', JSON.stringify(tasks))
+function storeLocalStorageData() {
+  localStorage.setItem('todolist-qNPXpg7Q', JSON.stringify(data))
+}
+
+function storeTask(task) {
+  data.tasks.push(task)
+  storeLocalStorageData()
 }
 
 function createTasksHTML() {
-  tasks.forEach((task, index) => {
-    createTaskHTML(task, index)
-  })
+  data.tasks.forEach(createTaskHTML)
 }
 
 function createTaskHTML(task, id) {
   const div = document.createElement('div')
   div.id = id
   div.classList.add('task')
+  const colors = task.color.split(' ')
+  div.style.background = colors[0]
+  if (colors[0] === '#111111') {
+    div.style.borderBottom = '2px solid #333'
+  } else {
+    div.style.border = '2px solid ' + colors[1]
+  }
   div.innerHTML += `<div class="task__done"><input type="checkbox" ${task.completed ? 'checked' : ''}>`
   div.innerHTML += `</div><div class="task__text">${task.title}</div>`
-  div.innerHTML += `<div class="task__important"><input type="checkbox" ${task.important ? 'checked' : ''}> Important</div>`
+  div.innerHTML += `<div class="task__important"><input type="checkbox" ${task.important ? 'checked' : ''}></div>`
   tasksContainer.appendChild(div)
 }
 
@@ -77,8 +88,8 @@ function newTask() {
   task.important = newTaskImportant.checked
   task.color = newTaskColor.value
   task.listID = newTaskList.value
-  storeTasks(task)
-  createTaskHTML(task)
+  storeTask(task)
+  createTaskHTML(task, data.tasks.indexOf(task))
   hideNewTaskDiv()
 }
 
@@ -90,7 +101,38 @@ function generalClickHandler(e) {
 }
 
 function optionsClickHandler(e) {
-  console.log(e.target.id)
+  const id = e.target.id
+  if (id === 'option-tasks') optionTasks()
+  if (id === 'option-important') optionImportant()
+  if (id === 'option-completed') optionCompleted()
+}
+
+function optionTasks() {
+  tasksHeader.textContent = 'Tasks'
+  for (const task of tasksContainer.children) {
+    task.classList.remove('hidden')
+  }
+}
+
+function optionImportant() {
+  tasksHeader.textContent = 'Important'
+  for (const task of tasksContainer.children) {
+    data.tasks[task.id].important ? task.classList.remove('hidden') : task.classList.add('hidden')
+  }
+}
+
+function optionCompleted() {
+  tasksHeader.textContent = 'Completed'
+  for (const task of tasksContainer.children) {
+    data.tasks[task.id].completed ? task.classList.remove('hidden') : task.classList.add('hidden')
+  }
+}
+
+function searchTask(e) {
+  const filter = e.target.value.toLowerCase()
+  for (const task of tasksContainer.children) {
+    data.tasks[task.id].title.toLowerCase().indexOf(filter) > -1 ? task.classList.remove('hidden') : task.classList.add('hidden')
+  }
 }
 
 function taskClickHandler(e) {
@@ -98,10 +140,27 @@ function taskClickHandler(e) {
 }
 
 function newTaskColorChange(e) {
-  e.target.style.background = e.target.value
+  e.target.style.background = e.target.value.split(' ')[0]
+}
+
+function addNewList(e) {
+  if (e.key === 'Enter') {
+    const list = e.target.value
+    if (list.length < 3 || list.length > 20) return
+
+    const div = document.createElement('div')
+    div.id = 'list-' + list
+    div.classList.add('options__option')
+    div.textContent = list
+    e.target.value = ''
+
+    e.target.before(div)
+  }
 }
 
 // Event listeners
+taskSearch.addEventListener('input', searchTask)
+optionsNewList.addEventListener('keydown', addNewList)
 mainContainer.addEventListener('click', generalClickHandler)
 newTaskColor.addEventListener('change', newTaskColorChange)
 newTaskCancel.addEventListener('click', hideNewTaskDiv)
